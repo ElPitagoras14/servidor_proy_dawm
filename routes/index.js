8ffa906f10306cac8dc8aab5f091219b63e4b770
Jonathan Garcia <jonfrgar@espol.edu.ec>
2022-01-11 18:20:59 -0500

    BD connected

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const sequelize = require("../models/index.js").sequelize;
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
+
+router.get("/autos", function (req, res, next) {
+  models.autos
+    .findAll({ attributes: {} })
+    .then(autos => {
+      res.send(autos);
+    })
+    .catch(err => res.status(400).send(err));
 });
 
 module.exports = router;
