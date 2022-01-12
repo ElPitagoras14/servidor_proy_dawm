51e4a0e5632a0ec0990d2aed26a26e0c69608d98
Jonathan Garcia <jonfrgar@espol.edu.ec>
2022-01-11 19:03:27 -0500

    Add login

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,29 @@ const sequelize = require("../models/index.js").sequelize;
 var initModels = require("../models/init-models");
 var models = initModels(sequelize);
 
-router.get("/autos", function (req, res, next) {
+router.get("/autos", (req, res, next) => {
   models.autos
     .findAll({ attributes: {} })
     .then(autos => {
       res.send(autos);
     })
     .catch(err => res.status(400).send(err));
+});
+
+/* POST para verificar usuario */
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  models.login
+    .findOne({ where: { correo: email, contrasenia: password } })
+    .then(cliente => {
+      if (cliente) {
+        res.send(cliente);
+      }
+      throw err;
+    })
+    .catch(err => {
+      res.send("Usuario no encontrado");
+    });
 });
 
 module.exports = router;
