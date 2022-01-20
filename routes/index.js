ce32102461a7f517e3bd5fdceebf69e1cc7e6b71
Jonathan Garcia <jonfrgar@espol.edu.ec>
2022-01-20 12:56:58 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,20 @@ router.get("/autos/:id", (req, res, next) => {
     .catch(err => res.status(400).send(err));
 });
 
+router.get("/promociones", (req, res, next) => {
+  models.promociones
+    .findAll({})
+    .then(promociones => res.send(promociones))
+    .catch(err => res.status(400).send(err));
+});
+
+router.get("/serv_mantenimiento", (req, res, next) => {
+  models.serviciomantenimiento
+    .findAll({})
+    .then(servicios => res.send(servicios))
+    .catch(err => res.status(400).send(err));
+});
+
 /* POST para verificar usuario */
 router.post("/login", (req, res, next) => {
   const { email, password } = req.body;
@@ -49,6 +63,50 @@ router.post("/login", (req, res, next) => {
     .catch(err => {
       res.send("Usuario no encontrado");
     });
+});
+
+router.post("/serv_grua", (req, res, next) => {
+  models.serviciogrua.create(req.body);
+  res.redirect("/");
+});
+
+router.post("/autos", (req, res, next) => {
+  models.autos.create(req.body);
+  res.redirect("/");
+});
+
+router.post("/promociones", (req, res, next) => {
+  models.promociones.create(req.body);
+  res.redirect("/");
+});
+
+router.post("/serv_mantenimiento", (req, res, next) => {
+  models.serviciomantenimiento.create(req.body);
+  res.redirect("/");
+});
+
+// DELETE
+router.delete("/usuarios/:id", (req, res, next) => {
+  models.usuarios
+    .findOne({ where: { id_usuario: req.params.id } })
+    .then(usuario => usuario.destroy())
+    .catch(err => res.status(400).send(err));
+  res.redirect("/");
+});
+
+router.delete("/autos/:id", (req, res, next) => {
+  models.autos
+    .findOne({ where: { id_auto: req.params.id } })
+    .then(auto => auto.destroy())
+    .catch(err => res.status(400).send(err));
+  res.redirect("/");
+});
+
+router.delete("/promociones/:id", (req, res, next) => {
+  models.promociones
+    .findOne({ where: { id_promocion: req.params.id } })
+    .then(promocion => promocion.destroy())
+    .catch(err => res.status(400).send(err));
 });
 
 module.exports = router;
