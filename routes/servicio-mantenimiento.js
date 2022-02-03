13397962e0e897c386d6bd35f16ee56524529884
ironpxs <joel.choez@hotmail.com>
2022-02-03 10:30:56 -0500

    Creación y actualización Proceso

--- routes/servicio-mantenimiento.js
+++ routes/servicio-mantenimiento.js
@@ -81,30 +81,6 @@ router.get("/procesos/:id_auto", (req, res, next) => {
     })
     .catch(error => res.status(400).send(error));
 });
-//PARA EL MECANICO
-router.get("/procesosmecanico/:id_auto", (req, res, next) => {
-  let consulta = `SELECT id_auto, GENERALT.id_proceso_siguiente, fecha_actual, hora_actual, observacion_mecanico, CONCAT(U.NOMBRES,' ',U.APELLIDOS) mecanico FROM 
-  ((SELECT id_proceso inicio, id_proceso id_proceso_siguiente FROM PROCESOMANTENIMIENTO P LIMIT 50)
-  UNION
-  SELECT id_proceso inicio, id_proceso_siguiente FROM PROCESOMANTENIMIENTO P WHERE P.ID_PROCESO_ANTERIOR IS NULL
-  UNION
-  SELECT INICIO.inicio, P.id_proceso_siguiente FROM PROCESOMANTENIMIENTO P, 
-  (SELECT id_proceso inicio, id_proceso_siguiente FROM PROCESOMANTENIMIENTO P WHERE P.ID_PROCESO_ANTERIOR IS NULL) INICIO 
-  WHERE INICIO.ID_PROCESO_SIGUIENTE = P.ID_PROCESO AND P.ID_PROCESO_SIGUIENTE IS NOT NULL) GENERALT 
-  JOIN SERVICIOMANTENIMIENTO SM ON SM.ID_PROCESO_INICIAL = GENERALT.INICIO
-  JOIN PROCESOMANTENIMIENTO PM ON PM.ID_PROCESO = GENERALT.ID_PROCESO_SIGUIENTE
-  JOIN USUARIOS U ON SM.ID_MECANICO = U.ID_USUARIO
-  WHERE id_auto = ${req.params.id_auto}
-  ORDER BY INICIO`;
-  sequelize
-    .query(consulta, {
-      type: sequelize.QueryTypes.SELECT,
-    })
-    .then(procesosfiltrados => {
-      res.send(procesosfiltrados);
-    })
-    .catch(error => res.status(400).send(error));
-});
 
 //POST
 router.post("/", (req, res, next) => {
@@ -117,6 +93,67 @@ router.get("/conteo/:id", (req, res, next) => {
   models.serviciomantenimiento
     .findAll({ where: { id_mecanico: id } })
     .then(datos => res.send({ cantidad: datos.length }));
+});
+
+//POST: PROCESOS
+router.post("/nuevoproceso/", (req, res, next) => {
+  var idprincipal = 0;
+  let consulta = `
+  SELECT GENERALT.id_proceso_siguiente FROM 
+  ((SELECT id_proceso inicio, id_proceso id_proceso_siguiente FROM PROCESOMANTENIMIENTO P LIMIT 50)
+  UNION
+  SELECT id_proceso inicio, id_proceso_siguiente FROM PROCESOMANTENIMIENTO P WHERE P.ID_PROCESO_ANTERIOR IS NULL
+  UNION
+  SELECT INICIO.inicio, P.id_proceso_siguiente FROM PROCESOMANTENIMIENTO P, 
+  (SELECT id_proceso inicio, id_proceso_siguiente FROM PROCESOMANTENIMIENTO P WHERE P.ID_PROCESO_ANTERIOR IS NULL) INICIO 
+  WHERE INICIO.ID_PROCESO_SIGUIENTE = P.ID_PROCESO AND P.ID_PROCESO_SIGUIENTE IS NOT NULL) GENERALT 
+  JOIN SERVICIOMANTENIMIENTO SM ON SM.ID_PROCESO_INICIAL = GENERALT.INICIO
+  JOIN PROCESOMANTENIMIENTO PM ON PM.ID_PROCESO = GENERALT.ID_PROCESO_SIGUIENTE
+  JOIN USUARIOS U ON SM.ID_MECANICO = U.ID_USUARIO
+  WHERE id_auto = ${req.body.id_auto}
+  ORDER BY id_proceso_siguiente DESC LIMIT 1;
+  `;
+  
+   sequelize
+    .query(consulta, {
+      type: sequelize.QueryTypes.SELECT,
+    })
+    .then(anterior => {
+      idprincipal = anterior[0].id_proceso_siguiente;
+        models.procesomantenimiento.create({
+        fecha_actual: req.body.fecha,
+        hora_actual: req.body.hora,
+        observacion_mecanico: req.body.observacion,
+        tipoMantenimiento: 2,
+        id_proceso_anterior: idprincipal
+        });
+
+      let consulta2 = `
+      select id_proceso from procesomantenimiento order by id_proceso desc limit 1;
+      `;
+      sequelize
+      .query(consulta2, {
+        type: sequelize.QueryTypes.SELECT,
+      })
+      .then(idupdate => {
+        console.log("llegó")
+        res.send(idupdate);
+        models.procesomantenimiento.update(
+          {
+          id_proceso_siguiente: idupdate[0].id_proceso + 1
+          },
+          { 
+          where: { id_proceso: idprincipal} 
+          }
+          );
+        });
+      
+    })
+    .catch(error => res.status(400).send(error));
+    
+    
+    
+    
 });
 
 //PUT
@@ -133,6 +170,7 @@ router.put("/", (req, res, next) => {
     .then(response => res.redirect("/"))
     .catch(err => res.status(500).send(err));
 });
+
 
 //DELETE
 router.delete("/", (req, res, next) => {
